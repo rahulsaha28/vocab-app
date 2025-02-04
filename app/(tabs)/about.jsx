4c20c0061e5babcb41b4b76e205b6e56887e8ef7
rahulsaha28 <saharahul039@gmail.com>
2025-02-05 01:27:33 +0600

    ADD:video

--- app/(tabs)/about.jsx
+++ app/(tabs)/about.jsx
@@ -1,10 +1,19 @@
+import { ResizeMode, Video } from "expo-av";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 const About = () => {
   return (
     <View>
-      <Text>About</Text>
+      <Video
+        source={{
+          uri:"https://github.com/rahulsaha28/eng-videos/raw/refs/heads/main/Chak_De_India.mp4"
+        }}
+        useNativeControls
+        style={{ width: "100%", height: 600 }}
+        resizeMode={ResizeMode.CONTAIN}
+        isLooping
+      />
     </View>
   );
 };
